d710c3091c9a809bfb8e1941a0b86ba5f6bd1cab
simsevenx <cooperation@qq.com>
2020-03-18 11:42:09 +0800

    1.1.6

--- reliable-message-center/reliable-schedule/src/main/java/io/xream/reliable/codetemplate/inner/ScheduleTemplateImpl.java
+++ reliable-message-center/reliable-schedule/src/main/java/io/xream/reliable/codetemplate/inner/ScheduleTemplateImpl.java
@@ -38,18 +38,21 @@ public class ScheduleTemplateImpl implements ScheduleTemplate {
         long startTime = System.currentTimeMillis();
         String taskId = loggingName + "_" + startTime;
 
-        logger.info("Executing " + loggingName + " At: " + new Date() + ", id: " + taskId);
+        logger.info("Executing {}, At: {}, id: {}", loggingName, new Date(), taskId);
 
         try {
             flag = callable.call();
         }catch (Exception e) {
-            logger.info("Exception Occured: " + ExceptionUtil.getMessage(e));
+            logger.info("Exception Occured: {}" ,ExceptionUtil.getMessage(e));
         }
 
         long endTime = System.currentTimeMillis();
 
         String result = flag == true?"OK," :  "FAILED,";
-        logger.info("Executing " + loggingName +", " + result +" Cost: " + (endTime - startTime) + "ms" + ", id: " + taskId);
+        logger.info("Executing {}, {}, Cost: {}ms id: {}",
+                loggingName, result,
+                (endTime - startTime),
+                taskId);
 
         return flag;
     }
